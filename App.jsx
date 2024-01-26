4cbfa59ff43287aacd87f519ee55a8fcfea8f938
Fahmi Malik Namus Akbar <89401669+fahmimna@users.noreply.github.com>
2024-01-26 13:34:33 +0700

    React

--- App.jsx
+++ App.jsx
@@ -1,31 +1,13 @@
-import { Slide, ToastContainer } from 'react-toastify'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import Navigation from './components/Navigation'
-import ContactPage from './pages/ContactPage'
+import 'preline/preline'
+import { RouterProvider } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import router from './router.js'
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Navigation />
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="contact" element={<ContactPage />} />
-        </Routes>
-      </BrowserRouter>
-
-      <ToastContainer
-        theme="colored"
-        hideProgressBar="true"
-        autoClose="3000"
-        draggable="false"
-        transition={Slide}
-      />
-    </>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
